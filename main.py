ec9040023479e91674f83d970249db2463d8863b
ruzannazohrabyan <ruzanna83@gmail.com>
2022-09-25 18:16:16 +0400

    implementing layered architecture

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from service.student_service import StudentService
 
 if __name__ == "__main__":
 
-    student_service = StudentService()
-    students = student_service.get_all()
+    student_svc = StudentService()
+    students = student_svc.get_all()
     print(students)
     print("\n*********************************\n")
-    student = student_service.get_by_username("ahovakimyan")
+    student = student_svc.get_by_username("ahovakimyan")
     print(student)
 
 
